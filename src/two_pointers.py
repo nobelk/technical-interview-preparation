e7ce82c130310953409ea37af17fe091b002a163
Dr. Nobel Khandaker <nobel@huskers.unl.edu>
2023-02-10 10:26:57 +0600

    Square sorted array problem.

--- src/two_pointers.py
+++ src/two_pointers.py
@@ -8,17 +8,40 @@ def find_pair_with_target_sum(arr, target_sum):
             return [numbers[target_sum - num], index]
         else:
             numbers[arr[index]] = index
-    return [-1, -1] # no result found
+    return [-1, -1]  # no result found
 
 
+# Given an array of sorted numbers, remove all duplicate number instances from it in-place, such that each element appears only once. The relative order of the elements should be kept the same and you should not use any extra space so that that the solution have a space complexity of O(1).
 def remove_duplicates(arr):
     if len(arr) <= 1:
         return -1
     next_unique_value = 1
     index = 0
     while index < len(arr):
-        if arr[next_unique_value-1] != arr[index]:
-            arr[next_unique_value-1] = arr[index]
+        if arr[next_unique_value - 1] != arr[index]:
+            arr[next_unique_value - 1] = arr[index]
             next_unique_value += 1
         index += 1
     return next_unique_value
+
+
+# Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+def square_sorted_array(arr):
+    if len(arr) < 2:
+        return [arr[0] * arr[0]]
+
+    rList = [0 for index in range(len(arr))]
+    leftIndex = 0
+    rightIndex = len(arr) - 1
+    highestIndex = len(arr) - 1
+
+    while leftIndex <= rightIndex:
+        if arr[leftIndex] * arr[leftIndex] > arr[rightIndex] * arr[rightIndex]:
+            rList[highestIndex] = arr[leftIndex] * arr[leftIndex]
+            leftIndex += 1
+        else:
+            rList[highestIndex] = arr[rightIndex] * arr[rightIndex]
+            rightIndex -= 1
+        highestIndex -= 1
+
+    return rList
--- test/test_solution.py
+++ test/test_solution.py
@@ -1,6 +1,7 @@
 import pytest
 from src.two_pointers import find_pair_with_target_sum
 from src.two_pointers import remove_duplicates
+from src.two_pointers import square_sorted_array
 
 
 def test_pair_with_target_sum():
@@ -12,3 +13,8 @@ def test_pair_with_target_sum():
 def test_remove_duplicates():
     assert remove_duplicates([2, 3, 3, 3, 3, 3, 6, 9, 11]) == 5
     assert remove_duplicates([2, 3, 3, 3, 3, 3, 6, 9, 11, 11, 12, 31]) == 8
+
+
+def test_square_sorted_array():
+    assert square_sorted_array([1, 2, 3]) == [1, 4, 9]
+    assert square_sorted_array([-10, -4, -1, 1, 2, 3]) == [1, 1, 4, 9, 16, 100]
