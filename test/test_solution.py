dda714c47354df5106534d7720fa8d396c390155
Dr. Nobel Khandaker <nobel@huskers.unl.edu>
2023-02-10 08:33:19 +0600

    Added additional test cases.

--- test/test_solution.py
+++ test/test_solution.py
@@ -4,3 +4,5 @@ from src.pair_with_target_sum import find_pair_with_target_sum
 
 def test_pair_with_target_sum():
     assert find_pair_with_target_sum([1, 2, 3, 4, 6], 6) == [1, 3]
+    assert find_pair_with_target_sum([2, 5, 9, 11], 11) == [0, 2]
+    assert find_pair_with_target_sum([2, 5, 9, 11], 15) == [-1, -1]
